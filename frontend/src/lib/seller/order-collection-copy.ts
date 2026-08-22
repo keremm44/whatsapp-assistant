a6712ad54d00510307fbed6f501357626e5baefd
keremm44 <keremm.4351@gmail.com>
2026-08-22 21:53:56 +0300

    Add order number requirement seller copy

---
+++ frontend/src/lib/seller/order-collection-copy.ts
@@ -0,0 +1,4 @@
+export const ORDER_NUMBER_REQUIRED_LABEL = "Sipariş numarası iste";
+
+export const ORDER_NUMBER_REQUIRED_HELP =
+  "Açık olduğunda asistan mevcut siparişi eşlemek için müşteriden sipariş numarasını ister.";
